759855719719622d91eac0f5b853d17150ceb9d7
Dhruvjit <dhruvjit@desk-now.com>
2022-11-29 22:58:56 +0530

    [Lec 85, 86 and 87] navigation bar component, router link and logo styling

--- ecommerce/src/routes/navigation/navigation.component.jsx
+++ ecommerce/src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,29 @@
-import {Outlet} from 'react-router-dom';
+import { Fragment } from 'react';
+import {Link, Outlet} from 'react-router-dom';
+import { ReactComponent as SiteLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = () => {
     return (
-      <div>
-        <div>
-          <h1> I am the navigation bar </h1>
+      /* Fragment hides the wrapping div from the dom when page renders */
+      <Fragment>
+        <div className='navigation'>
+          
+          {/* link to navigate to home */}
+          <Link className='logo-container' to='/'>
+            <SiteLogo className='logo'/>
+          </Link>
+          
+          {/*  */}
+          <div className="nav-links-container">
+            <Link className='nav-link' to='/shop'>
+              SHOP
+            </Link>
+          </div>
+
         </div>
         <Outlet/>
-      </div>
+      </Fragment>
     );
   };
 
