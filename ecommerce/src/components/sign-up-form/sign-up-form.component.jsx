5920fea33a6b498b7d22bad39062d0f0fb50d21a
Dhruvjit <dhruvjit@desk-now.com>
2022-12-02 07:19:43 +0530

    [lec 101] button styling fulfilled

--- ecommerce/src/components/sign-up-form/sign-up-form.component.jsx
+++ ecommerce/src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase.utils";
+import Button from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
     displayName: '',
@@ -60,8 +62,9 @@ const SignUpForm = () => {
     }
 
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className="sign-up-container">
+            <h2> Don't have an account? </h2>
+            <h1> Sign up with your email and password </h1>
             <form onSubmit={handleSubmit}>
                 
                 {/*  */}
@@ -108,9 +111,9 @@ const SignUpForm = () => {
                     }}
                 />
             
-                <button type="submit">
+                <Button type="submit">
                     Sign Up
-                </button>
+                </Button>
             
             </form>
         </div>
